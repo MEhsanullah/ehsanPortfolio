a491482aac86c08afc2c37729575ff0ec80b1a2c
Mr. Ehsanullah <muhammadehsanullah123@gmail.com>
2024-03-29 00:02:41 +0500

    First Upload

---
+++ src/data/constants.js
@@ -0,0 +1,297 @@
+export const Bio = {
+  name: "Muhammad Ehsanullah",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/MEhsanullah",
+  resume:
+    "https://drive.google.com/file/d/1EZ7qk7z2s--PjN3KfefqvaiIA9TMjk4L/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/muhammad-ehsanullah-50351a1b9/",
+  twitter: "https://twitter.com/xTLvJo6tQ5V2cH5",
+  insta: "https://www.instagram.com/mehsanullahx/",
+  facebook: "https://www.facebook.com/muhammad.ehsanullah.169/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: process.env.PUBLIC_URL + "/Images/html-logo.png",
+      },
+      {
+        name: "CSS",
+        image: process.env.PUBLIC_URL + "/Images/css-logo.png"
+      },
+      {
+        name: "Bootstrap",
+        image: process.env.PUBLIC_URL + "/Images/bs-logo.png"
+      },
+      {
+        name: "JavaScript",
+        image: process.env.PUBLIC_URL + "/Images/js-logo.png"
+      },
+      {
+        name: "JQuery",
+        image: process.env.PUBLIC_URL + "/Images/jquery-logo.png"
+      },
+      {
+        name: "React Js",
+        image: process.env.PUBLIC_URL + "/Images/react-logo.png"
+        },
+      {
+        name: "Tailwind CSS",
+        image: process.env.PUBLIC_URL + "/Images/tailwind-css.png"
+      },
+      {
+        name: "Angular JS",
+        image: process.env.PUBLIC_URL + "/Images/angular-logo.png"
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: process.env.PUBLIC_URL + "/Images/php-logo.png"
+      },
+      {
+        name: "Codeigniter",
+        image: process.env.PUBLIC_URL + "/Images/codeigniter-logo.png"
+      },
+      {
+        name: "ASP.NET Core",
+        image: process.env.PUBLIC_URL + "/Images/asp-logo.png"
+      },
+      {
+        name: "Node Js",
+        image: process.env.PUBLIC_URL + "/Images/node-js-logo.png"
+      },
+
+      {
+        name: "Express Js",
+        image: process.env.PUBLIC_URL + "/Images/express-logo.png"
+      },
+      {
+        name: "Python",
+        image: process.env.PUBLIC_URL + "/Images/Python-logo.png"
+     },
+      {
+        name: "MySQL",
+        image: process.env.PUBLIC_URL + "/Images/mysql_logo.png"
+      },
+  
+    ],
+  },
+  {
+    title: "Design UI/UX",
+    skills: [
+      {
+        name: "Adobe PS",
+        image: process.env.PUBLIC_URL + "/Images/ps-logo.png"
+      },
+      {
+        name: "Figma",
+        image: process.env.PUBLIC_URL + "/Images/figma-logo.png"
+      },
+
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: process.env.PUBLIC_URL + "/Images/git-logo.png"
+      },
+      {
+        name: "GitHub",
+        image: process.env.PUBLIC_URL + "/Images/github-logo.png"
+      },
+      {
+        name: "WordPress",
+        image: process.env.PUBLIC_URL + "/Images/WordPress-logo.png"
+      },
+      {
+        name: "Shopify",
+        image: process.env.PUBLIC_URL + "/Images/shopify-logo.png"
+      },
+      {
+        name: "VS Code",
+        image: process.env.PUBLIC_URL + "/Images/vscode-logo.png"
+     },
+      {
+        name: "Postman",
+        image: process.env.PUBLIC_URL + "/Images/postman-logo.png"
+      },
+    
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: process.env.PUBLIC_URL + "/Images/ambitsol-logo.jpeg",
+    role: "Full Stack Developer",
+    company: "Ambit Slutions",
+    date: "May 2021 - October 2022",
+    desc: "Starts from the frontend HTML,CSS and JavaScript then worked on PHP and MySQL.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Bootstrap",
+      "JavaScript",
+      "JQuery",
+      "PHP",
+      "Codeigniter",
+      "MySQL"
+    ],
+ 
+  },
+  {
+    id: 0,
+    img: process.env.PUBLIC_URL + "/digitalUx-logo.jpg",
+    role: "Wordpress Developer",
+    company: "Digital UX Lahore",
+    date: "May 2023 - Present",
+    desc: "Developing Interactive Websites from Wodpress and Shopify Stores",
+    skills: [
+      "Wodpress",
+      "Elementor",
+      "WooCommerce",
+      "Shopify",
+      "Liquid Coding",
+      "PHP",
+      "JavaScript"
+    ],
+   },
+  {
+    id: 1,
+    img: process.env.PUBLIC_URL + "/Images/ptcl-logo.png",
+    role: "Fullstack Internship",
+    company: "PTCL",
+    date: "July 2023 - September 2023",
+    desc: "Built a web App performing CRUD operations using Angular and .NET at backend.",
+    skills: [
+      "Angular JS",
+      "TypeScript",
+	  "JavaScript",  
+      "ASP.NET Web API's",
+      "MySQL",
+      "SSMS"
+    ],
+  },
+  {
+    id: 2,
+    img: process.env.PUBLIC_URL + "/swarm-logo.jpeg",
+    role: "Full Stack Developer Intern",
+    company: "Swarm Robotics Lab",
+    date: "September 2023 - Present",
+    desc: "Develope a frontend of the application that is used for the AI model to display it's Output",
+	skills: [
+      "React JS",
+	  "Bootstrap",
+	  "JavaScript"
+    ],
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: process.env.PUBLIC_URL + "/Images/uettaxila-logo.png",
+    school: "UET Taxila",
+    date: "September 2020  - Present",
+    grade: "3.32 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at University of Engineering and Technology, Taxila. I have completed 6 semesters and have a CGPA of 3.32. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also Software Head of the Taxilians Robotics and Automation Club (TRAC), where I am leading and working on exciting projects with a team of talented developers.",
+    degree: "BS Computer Engineering",
+  },
+  {
+    id: 1,
+    img:  process.env.PUBLIC_URL + "/Fg-logo.jpg",
+    school: "FG Quaid-e-Azam Degree College, Rawalpindi ",
+    date: "September 2018 - August 2020",
+    grade: "80%",
+    desc: "I completed my class 12 high school education at FG QADC, Rawalpindi, where I studied Science with Computer Science.",
+    degree: "HSSC Pre-Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Detailing Solutions",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "At DetailingSolutions, we specialize in providing exceptional web design and development services exclusively tailored for the detailing industry. With our deep understanding of your niche market and over 10 years of experience in the industry, we are your trusted partner in revitalizing and transforming your detailing business in the digital landscape.",
+    image:
+    process.env.PUBLIC_URL + "/Images/Detailing_solutions.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "JQuery",
+      "Wordpress",
+      "Elementor",
+      "Theme Customization",
+      "Form Handling"
+    ],
+    category: "Website",
+    github: "https://github.com/MEhsanullah",
+    webapp: "https://detailingsolutions.co/",
+  },
+  {
+    id: 0,
+    title: "DMS",
+    date: "Apr 2022 - Julu 2022",
+    description:
+      "A Distribution Management System (DMS) in PHP CodeIgniter is a robust software solution designed to streamline and optimize the processes involved in managing the distribution of goods. Leveraging the CodeIgniter framework, this system ensures efficient tracking, storage, and transportation of products across the supply chain. With user-friendly interfaces and responsive design, the DMS allows administrators to manage inventory, monitor shipments, and generate insightful reports. Integration with PHP CodeIgniter's powerful features enhances scalability and customization, making the Distribution Management System a reliable tool for businesses to enhance operational efficiency and meet customer demands effectively.",
+    image:
+    process.env.PUBLIC_URL + "/Images/dms-project.png",
+    tags: ["HTML", "CSS", "JavaScript", "JQuery", "PHP", "Codeigniter", "MySQL"],
+    category: "web app",
+    github: "https://github.com/MEhsanullah",
+    webapp: "#",
+  },
+  {
+    id: 1,
+    title: "ERP",
+    date: "Oct 2022 - January 2023",
+    description:
+      "The ERP Result Management System is a comprehensive solution developed in PHP CodeIgniter to effectively manage and streamline the results-related processes within an educational institution. Built on the robust CodeIgniter framework, this system facilitates the seamless entry, storage, and retrieval of student results. It offers administrators and educators an intuitive interface for result input, analysis, and reporting, enhancing overall efficiency in academic management. With features like result computation, grade generation, and customizable reporting, the ERP Result Management System ensures accuracy, transparency, and accessibility in handling academic performance data. Its scalability and modular design make it a valuable tool for educational institutions seeking a reliable and integrated solution for result management.",
+    image:
+    process.env.PUBLIC_URL + "/Images/erp-project.png",
+    tags: ["HTML", "CSS", "JavaScript", "JQuery", "PHP", "Codeigniter", "MySQL"],
+    category: "web app",
+    github: "https://github.com/MEhsanullah",
+    webapp: "#",
+  },
+  {
+    id: 2,
+    title: "Kochguru",
+    date: "October 2023 - Present",
+    description:
+"Kochguru Store on Shopify is a dynamic and user-friendly e-commerce platform tailored for culinary enthusiasts. Leveraging the power of Shopify, this project provides a seamless and visually appealing online shopping experience for customers interested in high-quality kitchen and cooking products. With a well-designed interface, intuitive navigation, and secure payment options, Kochguru Store ensures a convenient and enjoyable shopping journey. The Shopify platform's scalability and robust features enable easy product management, order processing, and customer engagement. Kochguru Store stands out as a reliable online destination for kitchen enthusiasts, offering a diverse range of products to elevate their culinary experience.",
+    image:
+    process.env.PUBLIC_URL + "/Images/kochguru.jpg",
+    tags: ["HTML", "CSS", "JavaScropit", "Liquid", "Shopify", "Customiztaion","Product Management"],
+    category: "Website",
+    github: "https://github.com/MEhsanullah",
+    webapp: "https://www.kochguru.ch/",
+  }
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
